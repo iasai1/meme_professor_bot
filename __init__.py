2b6bc04bceabb9c8708e71da13bfa54121fde97e
iasai1 <iasai1@yandex.ru>
2024-10-03 10:18:48 +0200

    Add captioning and recognition

--- __init__.py
+++ __init__.py
@@ -4,25 +4,29 @@ import time
 
 from embeddings import Embeddings
 
-# Directory containing your images
-image_dir = "C:\\Users\\iasai\\Downloads\\Telegram Desktop\\ChatExport_2024-09-27\\photos\\"
-image_paths = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
-embeddings = Embeddings()
+# # Directory containing your images
+# image_dir = "C:\\Users\\iasai\\Downloads\\Telegram Desktop\\ChatExport_2024-09-27\\photos\\"
+# image_paths = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
+embeddings = Embeddings(threshold=0.7)
 
-for img_path in image_paths:
-    user_id = random.randint(1, 5)
-    
-    # Generate random message_id (for example, a large random integer)
-    message_id = random.randint(100000, 999999)  # You can adjust this range as needed
-    
-    # Get the current time in milliseconds
-    datetime = int(time.time() * 1000)
-    user_id, message_id = embeddings.process_picture(img_path, user_id, message_id, datetime, keep_loaded=True, manual_flush=False)
-    if message_id > -1:
-        print(f"Duplicate found for {user_id}: message id {message_id}")
 
 embeddings.unload_db()
 my_image = "C:\\Users\\iasai\\Downloads\\Telegram Desktop\\ChatExport_2024-09-27\\photos\\photo_2@09-04-2023_13-40-01copy.jpg"
+
+files = ["photo_2024-09-20_13-20-22.jpg", "photo_2024-09-30_16-23-03 (2).jpg", "photo_6@09-04-2023_14-45-29.jpg", "photo_9@09-04-2023_14-45-29.jpg", "mirrored_image_output.jpg"]
+for img_path in files:
+    user_id = random.randint(1, 2)
+    img_path = "F:\\Users\\iasai\\Desktop\\" + img_path
+    
+    # Generate random message_id (for example, a large random integer)
+    message_id = random.randint(100000, 999999)  # You can adjust this range as needed
+    print(message_id)
+    
+    # Get the current time in milliseconds
+    datetime = int(time.time() * 1)
+    user_id, message_id = embeddings.process_picture(img_path, user_id, message_id, datetime, keep_loaded=True, manual_flush=True)
+    if message_id > -1:
+        print(f"Duplicate found for {user_id}: message id {message_id}")
 
 # test copy
 user_id = random.randint(1, 5)
@@ -35,6 +39,3 @@ for entry in embeddings.get_leaderboard():
     user_id = entry["user_id"]
     duplicate_count = entry["duplicate_count"]
     print(f"User ID: {user_id}, Duplicate Count: {duplicate_count}")
-
-
-
--- embeddings.py
+++ embeddings.py
@@ -1,23 +1,33 @@
-import json
 import pprint
-import numpy as np
 from PIL import Image
 from pymilvus import Collection, CollectionSchema, DataType, FieldSchema, connections
-from transformers import AutoImageProcessor, AutoModel
-from sklearn.metrics.pairwise import cosine_similarity
+from transformers import CLIPProcessor, CLIPModel, BlipProcessor, BlipForConditionalGeneration
 import torch
+import easyocr
+from paddleocr import PaddleOCR
+from PIL import Image
+
 
 class Embeddings:
 
-    def __init__(self, host="localhost", port="19530"):
-        # init model and processor
-        model_ckpt = "jayantapaul888/vit-base-patch16-224-finetuned-memes-v2"
-        self.processor = AutoImageProcessor.from_pretrained(model_ckpt)
-        self.model = AutoModel.from_pretrained(model_ckpt)
-        self.threshold = 0.99
+    def __init__(self, host="localhost", port="19530", threshold=0.99):
+
+        self.device = device="cuda" if torch.cuda.is_available() else "cpu"
+
+        # init CLIP model and processor for image and text embedding
+        img_model_ckpt = "openai/clip-vit-base-patch32"
+        self.clip_processor = CLIPProcessor.from_pretrained(img_model_ckpt)
+        self.clip_model = CLIPModel.from_pretrained(img_model_ckpt).to(device)
+
+        # Initialize BLIP model and processor for captioning
+        txt_model_ckpt = "Salesforce/blip-image-captioning-base"
+        self.txt_processor = BlipProcessor.from_pretrained(txt_model_ckpt)
+        self.txt_model = BlipForConditionalGeneration.from_pretrained(txt_model_ckpt).to(device)
 
         # a flag to keep state of embeddings collection in memory
         self.loaded = False
+
+        self.threshold = threshold
 
         # init db (milvus)
         connections.connect("default", host=host, port=port)
@@ -25,7 +35,7 @@ class Embeddings:
         # define schema for the embeddings collection
         embedding_fields = [
             FieldSchema(name="primary_id", dtype=DataType.INT64, is_primary=True, auto_id=True),
-            FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=768),
+            FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=512),
             FieldSchema(name="user_id", dtype=DataType.INT64),
             FieldSchema(name="message_id", dtype=DataType.INT64),
             FieldSchema(name="date", dtype=DataType.INT64)
@@ -38,7 +48,7 @@ class Embeddings:
             index_params = {
                 "index_type": "IVF_FLAT",
                 "metric_type": "IP",
-                "params": {"nlist": 100}
+                "params": {"nlist": 10}
             }
             self.embeddings_collection.create_index("embedding", index_params, index_name="idx_embeddings")
 
@@ -65,7 +75,7 @@ class Embeddings:
             self.loaded = True
             print("Loaded collection to memory")
 
-        r_user_id, r_message_id = self._process_picture(image_path, user_id, message_id, datetime, False) 
+        r_user_id, r_message_id = self._process_picture(image_path, user_id, message_id, datetime, manual_flush) 
 
         if not keep_loaded:
             self.unload_db()
@@ -87,7 +97,7 @@ class Embeddings:
         Returns:
             int, int: user_id and target_message_id. if target message is -1 - the image was not a duplicate
         """
-        new_embedding = self.get_image_embedding(image_path)
+        new_embedding = self.get_combined_image_text_embedding(image_path)
         print(image_path)
         
         duplicate_found = False
@@ -102,6 +112,8 @@ class Embeddings:
             limit=3, # grab top 3 just in case
             output_fields=["message_id"]
         )
+
+        pprint.pprint(results)
 
         for result in results:
             for i, distance in enumerate(result.distances):
@@ -118,18 +130,63 @@ class Embeddings:
         
         return user_id, target_message_id
     
+    def get_image_embedding(self, image):
+        """Get the image embedding using CLIP."""
+        inputs = self.clip_processor(images=image, return_tensors="pt").to(self.device)
 
-    def get_image_embedding(self, image_path):
-        """Process a single image and return its embedding."""
-        image = Image.open(image_path).convert("RGB")
-        inputs = self.processor(images=image, return_tensors="pt")
-        
         with torch.no_grad():
-            outputs = self.model(**inputs)
-            image_features = outputs.last_hidden_state[:, 0, :]
-            image_features /= image_features.norm(dim=-1, keepdim=True)
-        
+            image_features = self.clip_model.get_image_features(**inputs)
+            image_features /= image_features.norm(dim=-1, keepdim=True)  # Normalize to unit length
+
         return image_features.cpu().numpy().flatten()  # Flatten to 1D array
+
+    def get_text_embedding(self, description):
+        """Get the text embedding using CLIP."""
+        inputs = self.clip_processor(text=description, return_tensors="pt", padding=True).to(self.device)
+
+        with torch.no_grad():
+            text_features = self.clip_model.get_text_features(**inputs)
+            text_features /= text_features.norm(dim=-1, keepdim=True)  # Normalize to unit length
+
+        return text_features.cpu().numpy().flatten()  # Flatten to 1D array
+    
+    def generate_image_description(self, image):
+        """Generate a description for the image using BLIP."""
+        inputs = self.txt_processor(images=image, return_tensors="pt").to(self.device)
+        inputs = self.txt_processor(images=image, return_tensors="pt").to(self.device)
+        with torch.no_grad():
+            generated_ids = self.txt_model.generate(**inputs, max_length=200, num_beams=5, repetition_penalty=2.5, early_stopping=True)
+            description = self.txt_processor.decode(generated_ids[0], skip_special_tokens=True)
+
+        print(description)
+        return description
+
+    def get_combined_image_text_embedding(self, image_path):
+        """Get both image and text embeddings and return them combined."""
+        image = Image.open(image_path).convert("RGB")
+        image_embedding = self.get_image_embedding(image)
+
+        # Initialize EasyOCR reader
+        reader = easyocr.Reader(['en', 'ru'])  # Supports English ('en') and Russian ('ru')
+
+        # Perform OCR on the image
+        result = reader.readtext(image_path, detail=0)
+        print(result)
+
+        # Initialize PaddleOCR with language support
+        ocr = PaddleOCR(use_angle_cls=True, lang='cyrillic')  # English OCR
+
+        # Perform OCR
+        result = ocr.ocr(image_path)
+
+        # Print the recognized text
+        for line in result:
+            print("Detected Text:", [t[1][0] for t in line])
+        # text_embedding = self.get_text_embedding(self.generate_image_description(image))
+        # combined_embedding = (image_embedding + text_embedding) / 2
+
+        # return combined_embedding
+        return image_embedding
     
     
     def insert_embedding(self, embedding, user_id, message_id, datetime, manual_flush=False):
